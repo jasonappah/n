d64fccd251c2916568dcfe64698e0931d308b003
Jason Antwi-Appah <hey@jasonaa.me>
2021-02-19 05:05:43 +0000

    chore: remove github share button

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -33,7 +33,7 @@ import { PageHead } from './PageHead'
 import { PageActions } from './PageActions'
 import { Footer } from './Footer'
 import { PageSocial } from './PageSocial'
-import { GitHubShareButton } from './GitHubShareButton'
+// import { GitHubShareButton } from './GitHubShareButton'
 import { ReactUtterances } from './ReactUtterances'
 
 import styles from './styles.module.css'
@@ -278,7 +278,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }
       />
 
-      <GitHubShareButton />
+      {/* <GitHubShareButton /> */}
 
       <CustomHtml site={site} />
     </Twitter.Provider>
