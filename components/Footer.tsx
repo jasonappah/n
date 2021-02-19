cd59db3db955211233e74936622b13b2364a6e55
Jason Antwi-Appah <hey@jasonaa.me>
2021-02-19 05:08:17 +0000

    chore: disable theme switching

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
-import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
+// import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
 import * as config from 'lib/config'
 
 import styles from './styles.module.css'
@@ -11,26 +11,26 @@ export const Footer: React.FC<{
   isDarkMode: boolean
   toggleDarkMode: () => void
 }> = ({ isDarkMode, toggleDarkMode }) => {
-  const toggleDarkModeCb = React.useCallback(
-    (e) => {
-      e.preventDefault()
-      toggleDarkMode()
-    },
-    [toggleDarkMode]
-  )
+  // const toggleDarkModeCb = React.useCallback(
+  //   (e) => {
+  //     e.preventDefault()
+  //     toggleDarkMode()
+  //   },
+  //   [toggleDarkMode]
+  // )
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>Copyright {new Date().getFullYear()} {config.author}</div>
 
       <div className={styles.settings}>
-        <a
+        {/* <a
           className={styles.toggleDarkMode}
           onClick={toggleDarkModeCb}
           title='Tottle dark mode'
         >
           {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-        </a>
+        </a> */}
       </div>
 
       <div className={styles.social}>
