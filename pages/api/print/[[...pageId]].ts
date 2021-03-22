801683207f5ce329a3da336fc01bf00db93d2311
Jason Antwi-Appah <hey@jasonaa.me>
2021-03-22 14:45:32 -0500

    feat(print): fix dark mode, use human readable pdf title, allow for omission of last page

--- pages/api/print/[[...pageId]].ts
+++ pages/api/print/[[...pageId]].ts
@@ -1,13 +1,15 @@
 import Cors from 'cors'
 import { NextApiRequest, NextApiResponse } from 'next'
 import chromium from 'chrome-aws-lambda'
+import { PDFDocument, rgb } from 'pdf-lib'
 const cors = Cors()
 const fsPromises = require('fs').promises
 
 interface PDFGen {
-  page: String
-  path: String
+  page: string
+  path: string
   dark: boolean
+  omitFinalPage: boolean
 }
 
 // Helper method to wait for a middleware to execute before continuing
@@ -24,18 +26,23 @@ function runMiddleware(req, res, fn) {
   })
 }
 
+interface Pdf {
+  res: Buffer | Uint8Array
+  name: string
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors)
   const fallbackPageId = 'b7b46e3339f04662b52c7a700d22a338'
   const regex = /[^A-Za-z0-9]/
-  var pageId = ''
+  let pageId = ''
   try {
     pageId = req.query.pageId[0] as string
   } catch {
     pageId = fallbackPageId
   }
 
-  if (pageId == 'help' || pageId == 'h') {
+  if (pageId === 'help' || pageId === 'h') {
     pageId = fallbackPageId
   }
 
@@ -47,27 +54,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   console.log(req.query)
-  const darkMode = typeof req.query.dark === 'string' ? true : false
+  const darkMode = typeof req.query.dark === 'string'
+  const omitFinalPage = typeof req.query.omitFinalPage === 'string'
   const filename = `${pageId}${darkMode ? '-dark' : ''}.pdf`
   const folderPath = 'public/print'
   const filePath = `${folderPath}/${filename}`
 
   // 15 minutes -> convert to secs -> convert to millisecs
   const resetCacheMs = 15 * 60 * 1000
-
-  var pdf
-  var PAGE_EXISTS = true
+  let pdf: Pdf
+  let PAGE_EXISTS = true
 
   try {
     const printFolder = await fsPromises.readdir(folderPath)
-    // console.log(printFolder)
     const check = printFolder.includes(filename)
     if (check) {
       const stats = await fsPromises.lstat(filePath)
       if (new Date().getTime() - stats.mtime.getTime() > resetCacheMs) {
         PAGE_EXISTS = false
       } else {
-        pdf = await fsPromises.readFile(filePath)
+        const data = await fsPromises.readFile(filePath)
+        const doc = await PDFDocument.load(data)
+        pdf = { res: data, name: doc.getTitle() || 'Document' }
       }
     } else {
       PAGE_EXISTS = false
@@ -87,18 +95,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     pdf = await createPDF({
       page: pageId,
       path: filePath,
-      dark: darkMode
+      dark: darkMode,
+      omitFinalPage
     })
   } else {
     console.log('Pulling PDF from cache for file', filename)
   }
 
+  res.setHeader('Content-Disposition', `inline; filename="${pdf.name}.pdf"`)
   res.setHeader('Content-Type', 'application/pdf')
-  res.status(200).send(pdf)
+
+  res.status(200).send(Buffer.from(pdf.res))
 }
 
-async function createPDF(params: PDFGen) {
-  let browser, res
+async function createPDF(params: PDFGen): Promise<Pdf> {
+  let browser, res, fixedRes
 
   try {
     // add font support for emojis
@@ -108,7 +119,7 @@ async function createPDF(params: PDFGen) {
     )
 
     browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, '--no-sandbox', '--font-render-hinting=none'],
+      args: [...chromium.args, '--font-render-hinting=none'],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: true, // chromium.headless,
@@ -118,44 +129,78 @@ async function createPDF(params: PDFGen) {
     // make page into pdf and set res to be it.
     // also write the file to our cache
     const page = await browser.newPage()
-    await page.setViewport({ width: 1920, height: 1080 })
 
     const css = `body, div, article, header, p, h1, h2, h3, h4, h5, h6, a, span, footer, aside { font-family: "Inter", sans-serif !important; } div.notion-page-controls {margin-top: 15px !important; height: 0px !important;} div.notion-page-details-controls { padding-bottom: 5px !important; } div.notion-column_list-block > div > div { padding-top: 0px !important;} body > :last-child: { margin-bottom: -1px; overflow: hidden; }`
     const js = `document.head.insertAdjacentHTML("beforeend", '<link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"><style>${css}</style>')`
-    // console.log(js)
+    const getName = `document.getElementsByClassName("notion-page-block")[0].innerText`
     await page.goto(`https://notion.so/${params.page}`, {
       waitUntil: 'networkidle0'
     })
 
-    const darkModeKeys = ['Control', 'Shift', 'KeyL']
-
     if (params.dark) {
-      for (const key in darkModeKeys) {
-        await page.keyboard.down(key)
-      }
-      for (const key in darkModeKeys) {
-        await page.keyboard.up(key)
-      }
+      const darkModeKeys = ['Control', 'Shift', 'l']
+      darkModeKeys.map(async (press) => {
+        await page.keyboard.down(press)
+      })
     }
 
     await page.evaluate(js)
-
+    let name
+    try {
+      name = await page.evaluate(getName)
+    } catch {
+      name = 'Document'
+    }
     // just to make the page wait for the fonts to load in
     await page.screenshot()
 
     res = await page.pdf({
       format: 'Letter',
       displayHeaderFooter: false,
-      scale: 0.75
+      scale: 0.75,
+      printBackground: false,
+      margin: {
+        top: '0cm',
+        left: '0cm',
+        bottom: '0cm',
+        right: '0cm'
+      }
     })
+    const content = await PDFDocument.load(res)
+    const count = content.getPageCount()
 
-    return res
+    // this is awfully hacky, but like, it works.
+    // eventually i will figure out what is causing the white bars on the top and bottom of each page,
+    // but for now we'll leave it alone ;)
+    if (params.dark) {
+      const rect = {
+        x: 0,
+        y: 0,
+        width: 250,
+        height: 60,
+        borderWidth: 0,
+        color: rgb(47.0 / 255.0, 52.0 / 255.0, 55.0 / 255.0),
+        opacity: 1
+      }
+      content.getPages().map((page) => {
+        rect.width = page.getWidth()
+        page.drawRectangle(rect)
+        page.drawRectangle({ ...rect, y: page.getHeight() - rect.height })
+      })
+    }
+
+    if (params.omitFinalPage) {
+      content.removePage(count - 1)
+    }
+    content.setTitle(name)
+    fixedRes = await content.save()
+    return { res: fixedRes, name }
   } finally {
     if (browser) {
       await browser.close()
     }
     if (res) {
-      await fsPromises.writeFile(params.path, res)
+      await fsPromises.writeFile(params.path, fixedRes)
     }
   }
 }
