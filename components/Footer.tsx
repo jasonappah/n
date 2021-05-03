f813bbcb963fc4e7040a1168256973204fa4d208
Jason Antwi-Appah <hey@jasonaa.me>
2021-05-03 17:00:21 -0500

    chore: update deps + pull in upstream changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
-// import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
+import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
 import * as config from 'lib/config'
 
 import styles from './styles.module.css'
@@ -11,29 +11,34 @@ export const Footer: React.FC<{
   isDarkMode: boolean
   toggleDarkMode: () => void
 }> = ({ isDarkMode, toggleDarkMode }) => {
-  // const toggleDarkModeCb = React.useCallback(
-  //   (e) => {
-  //     e.preventDefault()
-  //     toggleDarkMode()
-  //   },
-  //   [toggleDarkMode]
-  // )
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const toggleDarkModeCb = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>
-        Copyright {new Date().getFullYear()} {config.author}
-      </div>
+      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
 
-      <div className={styles.settings}>
-        {/* <a
-          className={styles.toggleDarkMode}
-          onClick={toggleDarkModeCb}
-          title='Tottle dark mode'
-        >
-          {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-        </a> */}
-      </div>
+      {hasMounted ? (
+        <div className={styles.settings}>
+          <a
+            className={styles.toggleDarkMode}
+            onClick={toggleDarkModeCb}
+            title='Tottle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        </div>
+      ) : null}
 
       <div className={styles.social}>
         {config.twitter && (
